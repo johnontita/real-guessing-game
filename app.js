41914b9e1b59dd1043dca0841f4557c35721774d
john ontita <johnontita2000@gmail.com>
2024-06-06 06:29:53 +0300

    manipulating css using style property

--- app.js
+++ app.js
@@ -4,16 +4,27 @@ document.querySelector('.two').addEventListener('click',function(){
     const guess=Number(document.querySelector('.guess').value)
     console.log(guess,typeof guess)
      if(!guess){
-        //without using console.log() it fails to output 
-        console.log(document.querySelector('.message').textContent='no number')
-        
+         
+        document.querySelector('.message').textContent='no number'
+        //manipulating the css 
+        document.querySelector('body').style.backgroundColor='yellow'
+        document.querySelector('.value').style.backgroundColor='cyan'
+        document.querySelector('.value').style.width='10%'
 
     }
     else if(guess===secretNumber){
-       console.log(document.querySelector('.message').textContent='you are the winner🥰')
+       document.querySelector('.message').textContent='you are the winner🥰'
+       document.querySelector('body').style.backgroundColor='#3434'
+       document.querySelector('.value').style.backgroundColor='white'
     }
     else if(guess>secretNumber){
         document.querySelector('.message').textContent='the guess is higher than the secret number😒'
+        document.querySelector('body').style.backgroundColor='#6007'
+        
 
+    }
+    else if(guess<secretNumber){
+        document.querySelector('.message').textContent='the guess is low'
+        document.querySelector('body').style.backgroundColor='#5000'
     }
 })
