affc7f5e1a64eec394ca9a63d2bf8a4ba111c834
john ontita <johnontita2000@gmail.com>
2024-06-06 05:47:33 +0300

    using the console.log function to implement the game in DOM manipulation

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const secretNumber=Math.trunc(Math.random()*20)+1 //takes the range 1-20
 document.querySelector('.value').textContent=secretNumber // value is class name for the?
-document.querySelector('.btn-two').addEventListener('click',function(){
+document.querySelector('.two').addEventListener('click',function(){
     const guess=Number(document.querySelector('.guess').value)
     console.log(guess,typeof guess)
-    if(!guess){
-       document.querySelector('.message').textContent='no number'
+     if(!guess){
+        //without using console.log() it fails to output 
+        console.log(document.querySelector('.message').textContent='no number')
+        
 
     }
     else if(guess===secretNumber){
-        document.querySelector('.message').textContent='you are the winner🥰'
+       console.log(document.querySelector('.message').textContent='you are the winner🥰')
     }
     else if(guess>secretNumber){
-        document.querySelector('.message').textContent='the guess is high than the secret number'
+        document.querySelector('.message').textContent='the guess is higher than the secret number😒'
 
     }
 })
